04650795d7b9239ed9ad9b98ea06a8d3e78d8b30
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2024-10-16 00:48:32 +0000

    Initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">Morph Starter Kit</h1>
+        <p className="text-2xl text-muted-foreground">
+          Build your dapp frontends with the latest tools.
+        </p>
+      </section>
+    </main>
+  );
+}
